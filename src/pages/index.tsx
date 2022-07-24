af1dae814f4dc53390c5dd3d18f6d4c10bf4f083
class_moon <57883335+OrphanZhao@users.noreply.github.com>
2022-07-24 20:00:38 +0800

    add package urql

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,25 @@
 import styles from './index.less';
 import request from 'umi-request';
 
+let num = 5;
+
 export default function IndexPage() {
   // 1. 最原始的请求
   function fetchGraphql() {
     request
       .post('/api/graphql', {
         data: {
-          query: '{name}',
+          query: `
+            query NumRandom($num: Int!) {
+              getAtom(num: $num) {
+                numRandom(num: $num)
+                name
+                isNan
+                ids
+              }
+            }
+          `,
+          variables: { num },
         },
       })
       .then((res) => console.log('res', res));
